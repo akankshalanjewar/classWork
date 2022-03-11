ddf76f60482703c990594f299b0640b8e1964474
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-11 11:26:46 +0530

    added[akanksha]

---
+++ classwork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace classwork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name");
+            string Name = Console.ReadLine();  //Akanksha...
+            if (Name.Length < 3)
+            {
+                Console.WriteLine("Invalid User Entry");
+            }
+            else
+            {
+                Console.WriteLine(" Hello " + Name + " How are you....! ");
+
+            }
+        }
+    }
+}
